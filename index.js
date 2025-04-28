0c0dc3d828dbd0d7adc1353d752fd9fd021074dd
Dharansh-Neema <dharanshneema@gmail.com>
2025-04-29 02:19:10 +0530

    Mongodb connection tested

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
-
+const db_connection = require("./config/db");
 // DOTENV config
 require('dotenv').config()
 
+// DB connection
+db_connection();
+
 // PORT
 const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
