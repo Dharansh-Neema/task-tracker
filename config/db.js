8bc16319d9214f6bd4d1f20515e841e61a5520e0
Dharansh-Neema <dharanshneema@gmail.com>
2025-04-29 07:36:40 +0530

    DB config updated

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-
-const db_connection = () => {
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("DB Connected successfully"))
-    .catch((err) => console.log(err));
+const mongoose = require('mongoose')
+const db_connection = ()=>{
+    mongoose.set("strictQuery",false);
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(console.log("DB Connected successfully"))
+    .catch((err)=>{
+        console.error(err);
+        process.exit(1);
+    });
 }
 
 module.exports = db_connection;
